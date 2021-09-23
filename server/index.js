902be09dad3d1ce5c8541584a0a7176ad4172ee3
jalbiston <jacob.albiston@gmail.com>
2021-09-23 14:08:22 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+
+app.use(express.static("client"));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+const port = process.env.PORT || 4040
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`)
+})
